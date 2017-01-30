fe8a15097cae78fd8f846f2be2b125d8d820c57e
David <david.wiedenmann@gmail.com>
2017-01-29 22:06:40 -0800

    Add setxkbmap option for numpad:microsoft

---
+++ config/X11/xprofile.d/60-setxkbmap.sh
@@ -0,0 +1,9 @@
+# Set numeric keypad behavour so that shift <numeric key> works the same
+# as shift <cursor key> when numlock is off.
+setxkbmap -option numpad:microsoft
+
+# set caps lock by hitting both shift keys.
+#setxkbmap -option shift:both_capslock_cancel
+
+# or this one
+#setxkbmap -option shift:both_shiftlock
