1ffb45ed6ef1798d950ad013c70cef548f1c58cb
David <david.wiedenmann@gmail.com>
2017-01-29 22:40:23 -0800

    Fix .Xresources generate script to work with symlinked dotfiles
    Also fixed a bug where it wasn't correctly removing deleted config files from .Xresources

--- config/X11/xprofile.d/10-generate-xresources.sh
+++ config/X11/xprofile.d/10-generate-xresources.sh
@@ -13,43 +13,45 @@ RESOURCE_DIR=$( readlink -e ../Xresources.d/ )
 #RESOURCE_DIR="$HOME/.config/X11/Xresources.d/"
 XRESOURCES="$HOME/.Xresources"
 
-readlink $RESOURCE_DIR
-
 # Get a temporary file since it's a pain to handle all the
 # slashes and newlines otherwise using sed replace
 temp_file=$(mktemp)
 
-# find all files in resource dir, 
+# find all files in resource dir, (-L includes links that link link to files)
 # sort them, 
 # add #include and wrap in quotes
 # output to temp file
 # Count the # of Files we need to include 
 Cnt_Includes_Found=$( \
-find $RESOURCE_DIR -type f \
+find -L $RESOURCE_DIR -type f \
 	| sort \
 	| sed 's/.*/#include "&"/' \
 	| tee $temp_file \
 	| wc -l \
 	)
 
-cat $temp_file
-
 # could have just as easily reversed the order
-	# | tee >(wc -l) \
-	# > $temp_file 
+	#| tee >(wc -l) \
+	#> $temp_file \
+	#| tee $temp_file \
+	#| wc -l \
 
 # Count the # of existing matches in the file
 # Use | sort | uniq | wc to make sure we don't double count duplicate matches
 Cnt_Includes_Exist=$( \
-	grep -f $temp_file $XRESOURCES \
+	grep ^#include.*Xresources.d $XRESOURCES \
 	| sort | uniq | wc -l )
+
+#	grep -f $temp_file $XRESOURCES \
+#echo $Cnt_Includes_Found
+#echo $Cnt_Includes_Found
 
 if [ $Cnt_Includes_Found -ne $Cnt_Includes_Exist ]; then
 #if [ $REPLACE_XRESOURCES_INCLUDES ]; then
 	# Use sed to replace the existing auto-generated section of Xresources with this
 	lead='^! BEGIN GENERATED CONTENT$'
 	tail='^! END GENERATED CONTENT$'
-	sed -i -e "/$lead/,/$tail/{ /$lead/{p; r $temp_file
+	sed --follow-symlinks -i -e "/$lead/,/$tail/{ /$lead/{p; r $temp_file
 	        }; /$tail/p; d }"  $XRESOURCES
 fi
 
