e1b4b3eafa0317f2816e9f73ae127fceabad20a4
David Wiedenmann <david.wiedenmann@gmail.com>
2017-04-23 18:14:15 -0700

    i3 Efficiency Upgrade
    Setup Multiple Layouts
    Allow flexibility in setting wallpapers

--- bin/i3startup.sh
+++ bin/i3startup.sh
@@ -11,12 +11,16 @@
 # Execute main terminal in $ws_terminal
 #exec --no-startup-id i3-msg 'workspace $ws_terminal; exec $terminal'
 
-# Launch Composite Manager for transparency
-compton -b --config ~/.config/compton.conf
+#set +e
 
+# Launch Composite Manager for transparency
+if ! pgrep -x "compton" > /dev/null
+then
+    compton -b --config ~/.config/compton.conf
+fi
 
 # Network Manager Applet
-nm-applet
+nm-applet&
 
 # Dunst Notification Daemon
 #dunst -conf "/home/david/.config/dunstrc"
@@ -33,8 +37,41 @@ nm-applet
 # Set background
 #exec --no-startup-id nitrogen --restore
 
+
+
+# Launch Default Workspaces
+
+i3workspace.sh 21	& # Terminals
+sleep 2
+i3workspace.sh 22	& # Files
+sleep 2
+i3workspace.sh 23	& # Music
+sleep 2
+
+i3workspace.sh 11	& # Web
+sleep 2
+i3workspace.sh 12	& # Watch
+sleep 2
+i3workspace.sh 13	& # Git
+sleep 2
+
+
+i3workspace.sh 1	& # Explore
+sleep 2
+i3workspace.sh 2	& # Code
+sleep 2
+i3workspace.sh 3	& # Atom
+sleep 2
+
+
+
+
+i3-msg "workspace --no-auto-back-and-forth number 1"
+
 # Launch Rofi
 rofi -show run &
+
+#set -e
 
 ########################################################################################################################
 #	[-|\||) ()|= |_||\|[-
